6c92fa92954718b8d0ab565191fab808ed02eb7c
ShaikaJar <lorenz@karolo.de>
2023-01-09 15:43:13 +0100

    Build Compiler

--- src/processor/variableComp.ts
+++ src/processor/variableComp.ts
@@ -1,7 +1,6 @@
 import { Component } from "rete";
 import { NodeData, WorkerInputs, WorkerOutputs } from "rete/types/core/data";
-import { ProcessData } from "./processor";
 
 abstract class VariableComponent extends Component{
-    abstract worker(node: NodeData, inputs: WorkerInputs, outputs: WorkerOutputs, {stack, path}:ProcessData, ...args: unknown[]): void;
+    abstract worker(node: NodeData, inputs: WorkerInputs, outputs: WorkerOutputs, ...args: unknown[]): void;
 }
